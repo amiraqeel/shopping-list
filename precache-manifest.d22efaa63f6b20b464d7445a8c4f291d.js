e6cb8251dfff5738470e948f0d040d09f6b75147
amiraqeel <amir.aqeel123@gmail.com>
2020-06-05 15:23:05 +0530

    Updates

---
+++ precache-manifest.d22efaa63f6b20b464d7445a8c4f291d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7347fcf5c301020edfa768f81c967b01",
+    "url": "/shopping-list/index.html"
+  },
+  {
+    "revision": "7b2ad8a90de4a695bce8",
+    "url": "/shopping-list/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3bc6c4133612e38811e2",
+    "url": "/shopping-list/static/js/2.79087a0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-list/static/js/2.79087a0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2ad8a90de4a695bce8",
+    "url": "/shopping-list/static/js/main.01d408d6.chunk.js"
+  },
+  {
+    "revision": "420ca1ffb7049e8cad4d",
+    "url": "/shopping-list/static/js/runtime-main.4219e63f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shopping-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
